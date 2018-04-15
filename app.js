182ebb17b047e4bddc209a6a7eec08fab9b2e4cc
linder <linderhassinger00@gmail.com>
2018-04-15 09:48:44 -0500

    Final

--- app.js
+++ app.js
@@ -2,9 +2,25 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+
+
 var ruta = require('./rutas.js');
 http.createServer(function( req, res) {
     var pathname = url.parse(req.url).pathname;
     var r = new ruta(res, pathname);
+    var confirmar = fs.readFileSync('./confirmar.html');
+    var contacto = fs.readFileSync('./contacto.html');
 
-}).listen(9000);
+    if (req.method == "POST") {
+        req.on('data', function (data) {
+            dataString = +data
+        });
+        req.on('end', function () {
+           console.log("ES un metodo post");
+           res.end(confirmar.toString());
+        })
+    }
+    
+}).listen(9000, function () {
+    console.log("Se inicio el servicio");
+});
--- rutas.js
+++ rutas.js
@@ -8,81 +8,82 @@ var rutas = function(res, pathname) {
             res.end();
         });
  
-    }
-    if (pathname == '/nosotros') {
+    } else if (pathname == '/nosotros') {
         fs.readFile('./nosotros.html', function (error, html) {
             res.write(html);
             res.end();
         });
  
-    }
-    if (pathname == '/servicios') {
+    } else if (pathname == '/servicios') {
          fs.readFile('./servicio.html', function (error, html) {
              res.write(html);
              res.end();
          });
- 
-    }
-    if (pathname == '/contacto') {
+
+    } else if (pathname == '/contacto') {
          fs.readFile('./contacto.html', function (error, html) {
              res.write(html);
              res.end();
          });
-    }
-    if (pathname == '/img/nosotros.jpg') {
+    } else if (pathname == '/img/nosotros.jpg') {
          fs.readFile('./img/nosotros.jpg', function(error, html) {
              res.write(html);
              res.end();
          });
     }
-    if (pathname == '/img/inicio.jpg') {
+    else if (pathname == '/img/inicio.jpg') {
          fs.readFile('./img/inicio.jpg', function(error, html) {
              res.write(html);
              res.end();
          });
     }
-    if (pathname == '/img/servicio.jpg') {
+    else if (pathname == '/img/servicio.jpg') {
          fs.readFile('./img/servicio.jpg', function(error, html) {
              res.write(html);
              res.end();
          });
     }
-    if (pathname == '/img/contacto.jpg') {
+    else if (pathname == '/img/contacto.jpg') {
         fs.readFile('./img/contacto.jpg', function(error, html) {
             res.write(html);
             res.end();
         });
    }
-    if (pathname == '/otro/index.html') {
+   else if (pathname == '/otro/index.html') {
         fs.readFile('./otro/index.html', function(error, html) {
             res.write(html);
             res.end();
         });
     }
-    if (pathname == '/otro/css/bootstrap.js') {
+    else if (pathname == '/otro/css/bootstrap.js') {
         fs.readFile('./otro/css/bootstrap.js', function(error, html) {
             res.write(html);
             res.end();
         });
     }
-    if (pathname == '/img/team02.jpg') {
+    else if (pathname == '/img/team02.jpg') {
         fs.readFile('./img/team02.jpg', function(error, html) {
             res.write(html);
             res.end();
         });
     }
-    if (pathname == '/img/team03.jpg') {
+    else if (pathname == '/img/team03.jpg') {
         fs.readFile('./img/team03.jpg', function(error, html) {
             res.write(html);
             res.end();
         });
     }
-    if (pathname == '/img/thalia.jpeg') {
-        fs.readFile('./img/thalia.jpeg', function(error, html) {
+    else if (pathname == '/img/thalia.jpeg') {
+        fs.readFile('./img/thalia.jpeg', function (error, html) {
+            res.write(html);
+            res.end();
+        });
+    }else if (pathname != '/confirmar'){
+        fs.readFile('./erro.html', function(error, html) {
             res.write(html);
             res.end();
         });
     }
-}
+};
 
 module.exports = rutas;
